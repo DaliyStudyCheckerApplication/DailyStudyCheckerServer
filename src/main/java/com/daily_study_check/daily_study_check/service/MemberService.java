f37c42e8316f81c4993d342cdd0868eedb5a7700
hoho00 <jhoya963@gmail.com>
2022-02-14 20:41:00 +0900

    feat: add MemberRepository to MemberService

--- src/main/java/com/daily_study_check/daily_study_check/service/MemberService.java
+++ src/main/java/com/daily_study_check/daily_study_check/service/MemberService.java
@@ -2,6 +2,8 @@ package com.daily_study_check.daily_study_check.service;
 
 import org.springframework.stereotype.Service;
 
-public class MemberService {
+import com.daily_study_check.daily_study_check.repository.MemberRepository;
 
+public class MemberService {
+	private MemberRepository memberRepository;
 }
